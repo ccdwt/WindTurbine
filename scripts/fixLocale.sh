41a809a982ec94d457f1b790a120a7149a585360
MobiusHorizons <ukrcamilio@gmail.com>
2013-07-10 11:53:05 -0400

    added fixLocale.sh basically dos2unix

---
+++ scripts/fixLocale.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -e "/usr/bin/dos2unix2" ]; then
+	CMD="/usr/bin/dos2unix $1"
+elif [ -e /usr/bin/sed ]; then
+	CMD="/usr/bin/sed -i -e 's/\r$//' $1"
+elif [ -e /usr/bin/tr ]; then
+	CMD="/usr/bin/tr -d '\r' < $1 > tmp; chmod --reference=$1 tmp; mv tmp $1"
+else 
+	echo "ERROR: you do not appear to have either dos2unix, sed, or tr. Cannot continue";
+	exit 1;
+fi
+echo "cmd = $CMD"
+if [ -e $1 ]; then
+	$CMD
+else
+	echo "$0: $1: No such file or directory"
+	exit 1
+fi
